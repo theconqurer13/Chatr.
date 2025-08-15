39016d3f6f8f68398ea858b1f8b83529e8594df9
Aditya Jethvanth <jethvanth.aditya4536@gmail.com>
2025-08-15 23:02:30 +0530

    ..

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,12 +1,16 @@
 import mongoose from 'mongoose';
 
-const connectDb = async ()=>{
-    try {
-        mongoose.connection.on('connected',()=> console.log(" MongoDB connected successfully"));
-        await mongoose.connect(`${process.env.MONGODB_URL}/chatting-application`);
-        console.log('🚀 Database connection established')
-    } catch (error) {
-        console.log(error.message);
-    }
+const connectDb = async () => {
+  try {
+    mongoose.connection.on('connected', () => console.log("✅ MongoDB connected successfully"));
+    await mongoose.connect(`${process.env.MONGODB_URL}/chatting-application`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('🚀 Database connection established');
+  } catch (error) {
+    console.log("❌ DB connection error:", error.message);
+  }
 }
+
 export default connectDb;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 import connectDb from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from './controllers/clerkWebhooks.js';
